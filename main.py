344a7fd7f4d0b72855d656b3587fd95d3814d423
Carson Duffy <cwduffy01@gmail.com>
2023-02-26 01:49:26 -0600

    documentation

--- main.py
+++ main.py
@@ -14,27 +14,23 @@ import json
 from math import *
 
 class MyNode(Node):
-
-    max_x = 11.088889122009277
-    max_y = 11.088889122009277
-
+    # variables to track the robot's state
     turtle_x = 0
     turtle_y = 0 
     turtle_theta = 0
 
-    points = []
-    current_target = 0
+    points = []         # list of waypoints for navigation
+    current_target = 0  # index of the target point in the list
 
-    rotate = True
-    rot_vel = 0
+    rotate = True   # state control variable for rotating vs. translating
 
-    timer_delay = 0.1
-
+    # pid parameters
     rotate_error_thresh = 0.0001
     translate_error_thresh = 0.01
-    kp = 3.0
+    kp = 5.0
     
     def __init__(self, node_name):
+        # get points from json file
         with open("points.json", 'r') as f:
             self.points = json.load(f)
 
@@ -64,33 +60,36 @@ class MyNode(Node):
 
     def pub_sub_callback(self, future):
         self.publisher_ = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)  # publish Twist message to cmd_vel topic
-        # timer_delay = 0.1
-        self.timer = self.create_timer(self.timer_delay, self.timer_callback)  # publisher callback executes every one second
+        timer_delay = 0.1
+        self.timer = self.create_timer(timer_delay, self.timer_callback)  # publisher callback executes every one second
         self.subscription = self.create_subscription(Pose, "/turtle1/pose", self.listener_callback, 10)    # listen to color sensor topic for Color message
 
-    
     def timer_callback(self):
+        # leave function if there are no points left
         if self.current_target == len(self.points):
             return
 
-        # initialize message and change linear/angular velocity
+        # initialize message to send
         msg = Twist()
 
+        # get target point
         target = self.points[self.current_target]
 
         if self.rotate:
+            # calculate target angle and set pid
             angle = atan2(target[1] - self.turtle_y, target[0] - self.turtle_x)
             error = self.turtle_theta - angle
             msg.angular.z = -self.kp * error    
 
             print(f"ERROR: {error}")
 
+            # change state variable to translate next
             if abs(error) < self.rotate_error_thresh:
                 print("ending rotation")
-                self.rot_vel = 0
                 self.rotate = False
 
         else:
+            # calculate target distance and set pid
             error_x = self.turtle_x - target[0]
             error_y = self.turtle_y - target[1]
             error = -((error_x) ** 2 + (error_y) ** 2) ** 0.5
@@ -98,35 +97,25 @@ class MyNode(Node):
 
             print(f"ERROR: {error}")
 
+            # change state variable to rotate next
             if abs(error) < self.translate_error_thresh:
                 print("ending translation")
                 self.rotate = True
                 self.current_target += 1
-
-        # print(f"Message Received - x: {self.turtle_x}, y: {self.turtle_y}, theta: {self.turtle_theta}")
         
         self.publisher_.publish(msg)    # publish the message
     
+    # set pose of turtle to node's instance variables when messages are published to /cmd_vel
     def listener_callback(self, msg):
         self.turtle_x = msg.x
         self.turtle_y = msg.y
         self.turtle_theta = msg.theta
-        # self.get_logger().info(f"Message Received - x: {msg.x}, y: {msg.y}, theta: {msg.theta}")  # output message to console
         
 def main(args=None):
     rclpy.init(args=args)
     
     # create node and spin
     my_node = MyNode("pub_sub_node")
-
-    # executor = rclpy.executors.MultiThreadedExecutor()
-    # executor.add_node(my_node)
-    # executor.add_node(params)
-    # executor_thread = threading.Thread(target=executor.spin, daemon=True)
-    # executor_thread.start()
-
-
-    # rclpy.spin(params)
     rclpy.spin(my_node)
     
     # destory node and shut down application
